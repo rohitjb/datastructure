3c5acb967771aaf3e7b323a25629deaad53cadf4
rohit <rohit0690@gmail.com>
2020-02-09 22:19:57 +1100

    implement auicksort

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-	selectionSort "github.com/rohitjb/datastructure/sorting/selection"
+	"fmt"
+	quickSort "github.com/rohitjb/datastructure/sorting/quick"
 )
 
 func main() {
 	//sort.BubbleSortAlgo([]int{8, 5, 7, 3, 2})
 	//insertionSort.Sort([]int{8, 5, 7, 3, 2, 1, 6, 4})
-	selectionSort.Sort([]int{8, 5, 7, 3, 2, 1, 6, 4})
+	//selectionSort.Sort([]int{8, 5, 7, 3, 2, 1, 6, 4})
+	arr := []int{50, 70, 60, 90, 40, 80, 10, 20, 30}
+	quickSort.Sort(arr, 0, len(arr))
+	fmt.Println(arr)
 }
--- sorting/quick/quickSort.go
+++ sorting/quick/quickSort.go
@@ -1,5 +1,47 @@
 package quick
 
-func Sort(arr []int) {
+func Sort(arr []int, l int, h int) {
+	if l < h {
+		j:= partition(arr, l, h)
+		Sort(arr, l, j)
+		Sort(arr, j+1, h)
+	}
+}
 
+func partition(arr []int, l int, h int) int {
+	pivot := arr[l]
+	// look for larger element than pivot
+	i := l
+	j := h
+	for{
+		for {
+			i = i + 1
+			if i >= len(arr) - 1 || arr[i] >= pivot {
+				break
+			}
+		}
+		// look for smaller element than pivot
+		for {
+			j = j - 1
+			if j <= 0 || arr[j] <= pivot {
+				break
+			}
+		}
+		// swap with i and j
+		if i < j  {
+			temp := arr[i]
+			arr[i] = arr[j]
+			arr[j] = temp
+		}
+
+		if i > j {
+			break
+		}
+	}
+
+	temp := arr[l]
+	arr[l] = arr[j]
+	arr[j] = temp
+
+	return j
 }
