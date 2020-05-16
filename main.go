1f177fb1d1c68c24345b90708e1e9baa673b4f68
Rohit Rohit <rohit.rohit@anz.com>
2020-05-17 08:00:23 +1000

    add dockerfile

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"fmt"
-	quickSort "github.com/rohitjb/datastructure/sorting/quick"
+	sort "github.com/rohitjb/datastructure/sorting/bubble"
 )
 
 func main() {
-	//sort.BubbleSortAlgo([]int{8, 5, 7, 3, 2})
+	sort.BubbleSortAlgo([]int{8, 5, 7, 3, 2})
 	//insertionSort.Sort([]int{8, 5, 7, 3, 2, 1, 6, 4})
 	//selectionSort.Sort([]int{8, 5, 7, 3, 2, 1, 6, 4})
-	arr := []int{50, 70, 60, 90, 40, 80, 10, 20, 30}
-	quickSort.Sort(arr, 0, len(arr))
-	fmt.Println(arr)
+
+	//arr := []int{50, 70, 60, 90, 40, 80, 10, 20, 30}
+	//quickSort.Sort(arr, 0, len(arr))
+	//fmt.Println(arr)
 }
